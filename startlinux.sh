3e0aa83217e1e331fa00b1f95b1633912ab2db67
shimmer24 <60858919+shimmer24@users.noreply.github.com>
2020-02-13 04:48:28 +0300

    Create startlinux.sh

---
+++ startlinux.sh
@@ -0,0 +1,21 @@
+os="ubuntu" 
+username="${os}" 
+unset LD_PRELOAD
+proot \
+  -0 \
+  --link2symlink \
+  -r ~/$os \
+  -b /dev/ \
+  -b /sys/ \
+  -b /proc/ \
+  -b /sdcard/ \
+  -b /storage/ \
+  /usr/bin/env \
+  -i \
+    HOME=/root \
+    PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games \
+    TERM="xterm-256color" \
+    USER=${username} \
+    USERNAME=${username} \
+    /usr/bin/bash --logl
+    
