709cf08f3ead10c60c0cece58102aa5f44b4caf0
shimmer24 <60858919+shimmer24@users.noreply.github.com>
2020-02-19 21:42:57 +0300

    Add Android data

--- startlinux.sh
+++ startlinux.sh
@@ -1,12 +1,13 @@
 os="ubuntu" 
-username="${os}" 
+username="linux" 
 unset LD_PRELOAD
 proot \
   -0 \
   --link2symlink \
-  -r ~/${os} \
+  -r ~/$os \
   -b /dev/ \
   -b /sys/ \
+  -b /data/ \
   -b /proc/ \
   -b /sdcard/ \
   -b /storage/ \
@@ -15,7 +16,7 @@ proot \
     HOME=/root \
     PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games \
     TERM="xterm-256color" \
-    USER=${username} \
-    USERNAME=${username} \
-    /usr/bin/bash --logl
-    
+    USER=$username \
+    USERNAME=$username \
+    ANDROID_DATA=/data \
+    /usr/bin/bash --login
